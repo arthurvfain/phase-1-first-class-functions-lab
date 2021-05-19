3d5c6e9710bbb1f4375d988bdc9dfcb2e32046b1
Arthur V Fain <arthurvfain@gmail.com>
2021-05-19 08:51:28 -0400

    commit tree 0470536ba465c6730977a18d81145b0e98b67f7b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drive)
+{
+    const drivers = () => [drive[0], drive[1]];
+    return drivers();
+}
+function returnLastTwoDrivers(drive)
+{
+    const drivers = () => [drive[drive.length-2], drive [drive.length-1]];
+    return drivers();
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(x)
+{
+    return (fare) => (fare * x);
+}
+function fareDoubler(j)
+{
+    const twiced = createFareMultiplier(2);
+    return twiced(j);
+}
+function fareTripler(j)
+{
+    const tripled = createFareMultiplier(3);
+    return tripled(j);
+}
+function selectDifferentDrivers(rock, hardPlace)
+{
+    return hardPlace(rock);
+}
